7f02ed0bcadffc98635e08868950081c76793822
Xinmin Du <10803082+doris-xm@user.noreply.gitee.com>
2024-07-04 17:23:21 +0800

    bug: default route

--- idc_bgp.py
+++ idc_bgp.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import time
 from mininet.cli import CLI
 from mininet.net import Mininet
@@ -40,13 +41,13 @@ router bgp {asnum}
     for neighbor in neighbors:
         # config += f" neighbor {neighbor['ip']} interface remote-as external\n"
         config += f" neighbor {neighbor['ip']} remote-as {neighbor['as']}\n"
-        config += f" neighbor {neighbor['ip']} ebgp-multihop 255\nneighbor {neighbor['ip']} timers 5 5\naddress-family ipv4 unicast\n"
+        config += f" neighbor {neighbor['ip']} ebgp-multihop 255\n neighbor {neighbor['ip']} advertisement-interval 10\n"
     config += f"""
 log file /tmp/{name}.log
 """
     return config
 
-def configureBGP(net, n_leaf=2, n_spine=2, m_hosts=2):
+def configureBGP(net, n_leaf=2, n_spine=2, m_hosts=2, host_links=2):
     """Configure FRRouting on all routers."""
     all_spine = [{'name': router.name, 'ip': router.IP(), 'as': router.params['asnum']} for router in net.hosts if 'spine' in router.name]
     all_leaf = [{'name': router.name, 'ip': router.IP(), 'as': router.params['asnum']} for router in net.hosts if 'leaf' in router.name]
@@ -69,9 +70,9 @@ def configureBGP(net, n_leaf=2, n_spine=2, m_hosts=2):
                     spine_id = int(spine['name'][-1])
                     neighbors.append({'name': spine['name']+'-eth{}'.format(leaf_id-1),
                                       'ip':  f'192.168.{n_leaf*(spine_id-1) + leaf_id}.1', 'as': spine['as']})
-                for h in range(m_hosts):
-                    neighbors.append({'name': f'h{m_hosts*(leaf_id-1)+h+1}-eth0',
-                                      'ip': f'192.168.{n_leaf*n_spine + (leaf_id-1)*m_hosts + h+1}.2', 'as':64000+h+1})
+                # for h in range(m_hosts*host_links):
+                #     neighbors.append({'name': f'h{m_hosts*(leaf_id-1)+h+1}-eth0',
+                #                       'ip': f'192.168.{n_leaf*n_spine + (leaf_id-1)*m_hosts + h+1}.1', 'as':'64000+h+1'})
 
             # if 'spine' in router.name:
             #     network = '192.168.1.{}/32'.format(router.name[-1])
@@ -140,9 +141,10 @@ class SpineLeafTopo(Topo):
         for j in range(m_hosts):
             host = self.addHost(f'h{j+1}',
                                 ip=f'192.168.{n_leaf*n_spine + j*host_links+1}.1/24',
-                                asnum=64000 + j + 1)
-            leaf_switch1 = leaf_switches[(j+1)%n_leaf+1]
-            leaf_switch2 = leaf_switches[(j+2)%n_leaf+1]
+                                asnum=64000 + j + 1,
+                                defaultRoute=f'via 192.168.{n_leaf*n_spine + j*host_links+1}.2')
+            leaf_switch1 = leaf_switches[(j)%n_leaf+1]
+            leaf_switch2 = leaf_switches[(j+1)%n_leaf+1]
             self.addLink(host, leaf_switch1,
                          intfName1 = '{}-eth0'.format(host), params1 = {'ip': f'192.168.{n_leaf*n_spine + j*host_links+1}.1/24'},
                          # intfName2='{}-eth{}'.format(leaf_switch, j),
@@ -155,7 +157,34 @@ class SpineLeafTopo(Topo):
                         )
 
 
-def perfTest(lossy=True):
+def iperfTest(net):
+    "Run iperf between h1 and h2"
+    h1, h2 = net.get('leaf1', 'leaf4')
+    print("Starting iperf server...")
+    h1.cmd('iperf -s &')
+    print("Starting iperf client...")
+    iperf_output = h2.cmd('iperf -c', h1.IP())
+    print(iperf_output)
+
+def log_spine_routes(node,name):
+    # len = -1
+    last_content = ""
+    while True:
+        content = node.cmd("route -n\n")
+        # len = content.count("255.255.255.0")
+        write = content
+        if content != last_content:
+            with open(f"{prefix_dir}{name}_routes.log", "a") as log_file:
+                log_file.write(f"{time.time()} \n {write}\n")
+        else:
+            with open(f"{prefix_dir}{name}_routes.log", "a") as log_file:
+                log_file.write(f"{time.time()} \n\n")
+        last_content = content
+
+
+        time.sleep(1)
+
+def run(lossy=True):
     n_spine = 2
     n_leaf = 4
     m_hosts = 4
@@ -172,13 +201,47 @@ def perfTest(lossy=True):
     info("Dumping host connections\n")
     dumpNodeConnections(net.hosts)
     for node in net.hosts:
+        if 'h' in node.name:
+            node.setDefaultRoute(f'via 192.168.{n_leaf*n_spine + (int(node.name[1:])-1)*host_links+1}.2')
+            print(f"Node: {node.name} default route: {f'via 192.168.{n_leaf*n_spine + (int(node.name[1:])-1)*host_links+1}.2'}")
         print(f"Node: {node.name}")
         for intf in node.intfList():
             print(f"  Interface: {intf.name}, IP: {intf.IP()}")
 
-    configureBGP(net)
+    configureBGP(net, n_leaf=n_leaf, n_spine=n_spine, m_hosts=m_hosts, host_links=host_links)
     net.start()
+    # 开一个线程，每1秒log spine1 的路由
+    spine1 = net.getNodeByName('spine2')
+    # spine2 = net.getNodeByName('spine2')
+    # leaf1 = net.getNodeByName('leaf1')
+    # leaf2 = net.getNodeByName('leaf2')
+    # leaf3 = net.getNodeByName('leaf3')
+    # leaf4 = net.getNodeByName('leaf4')
+
+    # 创建并启动日志线程
+    log_thread1 = threading.Thread(target=log_spine_routes, args=(spine1,'spine2'))
+    # log_thread2 = threading.Thread(target=log_spine_routes, args=(spine2,'spine2'))
+    # log_thread3 = threading.Thread(target=log_spine_routes, args=(leaf1, 'leaf1'))
+    # log_thread4 = threading.Thread(target=log_spine_routes, args=(leaf2, 'leaf2'))
+    # log_thread5 = threading.Thread(target=log_spine_routes, args=(leaf3, 'leaf3'))
+    # log_thread6 = threading.Thread(target=log_spine_routes, args=(leaf4, 'leaf4'))
+
+    log_thread1.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    # log_thread2.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    # log_thread3.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    # log_thread4.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    # log_thread5.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    # log_thread6.daemon = True  # 设置为守护线程，这样在主程序退出时该线程也会退出
+    log_thread1.start()
+    # log_thread2.start()
+    # log_thread3.start()
+    # log_thread4.start()
+    # log_thread5.start()
+    # log_thread6.start()
+
+
     CLI(net)
+
     net.stop()
     os.system(f'rm -f /tmp/bgpd* /tmp/*.api  /tmp/zebra* /tmp/*.log {prefix_dir}logs/*')
     os.system("killall -9 bgpd zebra")
@@ -186,4 +249,4 @@ def perfTest(lossy=True):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    perfTest(lossy=False)
+    run(lossy=False)
